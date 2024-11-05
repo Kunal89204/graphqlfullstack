d5a503d6c6da5df17eb7907e8a694a8ff1ddc2b3
kunal89204 <kunalkhandelwal108@gmail.com>
2024-11-05 11:10:28 +0530

    vercel.json added

--- server/app.js
+++ server/app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express();
 const cors = require("cors")
 const bodyParser = require("body-parser")
+const dotenv = require("dotenv")
 app.use(cors())
-
+dotenv.config()
 app.use(bodyParser.json())
 
 
